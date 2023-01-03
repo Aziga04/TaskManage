0b23a179418166ea5b730f08ba420e330950e09b
Aziga04 <azizmusaev33@gmail.com>
2023-01-03 14:39:50 +0600

    add

--- app/src/main/java/com/example/taskmanage/MainActivity.kt
+++ app/src/main/java/com/example/taskmanage/MainActivity.kt
@@ -10,26 +10,32 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.taskmanage.Data.Pref
 import com.example.taskmanage.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val pref = Pref(this)
+        val auth = FirebaseAuth.getInstance()
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         if (!pref.isOnBoardingSeen())
             navController.navigate(R.id.onBoardingFragment2 )
+
+        if (auth.currentUser == null){
+            navController.navigate(R.id.authFragment)
+        }
 
         val appBarConfiguration = AppBarConfiguration(setOf(
             R.id.navigation_home, R.id.navigation_dashboard,
@@ -44,7 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { controller, destination, arguments ->
             navView.isVisible = navFragment.contains(destination.id)
-            if (destination.id == R.id.onBoardingFragment2 ) {
+            if (destination.id == R.id.onBoardingFragment2 ||destination.id == R.id.authFragment) {
                 supportActionBar?.hide()
             } else supportActionBar?.show()
 
